d535102df90bdc17c651abdd2de1a59827bc0a33
PouleRequest <poulerequest@gmail.com>
2014-03-10 11:27:25 +0100

    Added the "List surveys" feature

--- protected/controllers/SurveysController.php
+++ protected/controllers/SurveysController.php
@@ -31,6 +31,14 @@ class SurveysController extends Controller
 		));
 	}
 
+		public function actionIndex()
+		{
+			$dataProvider=new CActiveDataProvider('Survey');
+			$this->render('index',array(
+				'dataProvider'=>$dataProvider,
+			));
+		}
+
 	/**
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.
