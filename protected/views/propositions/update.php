85d7cfe97d91b0a6ce883dd83297bdeb269ba55d
Steven Catteaux <stevenrvn@gmail.com>
2014-05-09 08:52:33 +0200

    Added propositions position update

--- protected/views/propositions/update.php
+++ protected/views/propositions/update.php
@@ -3,7 +3,11 @@
     echo CHtml::beginForm();
         echo CHtml::label('Titre de la proposition', 'Proposition[title]');
         echo CHtml::textField('Proposition[title]', $proposition->title);
-        echo CHtml::submitButton('Envoyer');
+        echo '<br/>';
+        echo CHtml::label('Position de la proposition', 'Proposition[position]');
+        echo CHtml::textField('Proposition[position]', $proposition->position);
+        echo '<br/>';
+        echo CHtml::submitButton('Mettre à jour');
     echo CHtml::endForm();
     
 ?>
